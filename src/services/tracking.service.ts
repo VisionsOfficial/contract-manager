3ab3ba2f200291f0290e91e0eb464a86f63537aa
VISIONS-David <david@visionspol.eu>
2025-08-04 09:34:11 +0200

    refactor : forgotten debug erasure

--- src/services/tracking.service.ts
+++ src/services/tracking.service.ts
@@ -15,15 +15,11 @@ export class TrackingService {
     participantId: string,
   ) {
     if (process.env.USE_ACTION_TRACKING === 'true') {
-      console.log('contractVerification : ', contractId, participantId);
       const route = `${process.env.CATALOG_URL}/tracking/contract-verification`;
-      console.log('Route : ', route);
       const response = await axios.post(
         `${process.env.CATALOG_URL}/tracking/contract-verification`,
         { contractId, participantId },
       );
-      console.log('response : ', response.status);
-      console.log('response : ', response.data);
     }
   }
 }
